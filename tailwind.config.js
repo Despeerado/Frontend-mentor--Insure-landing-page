7e1cdf4944d11e80d3ac5b4144c9b530db0e8b93
Dominik Beran <d.beran27@gmail.com>
2023-09-12 18:33:21 +0200

    set up fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     screens: {
       'sm': '640px',
@@ -8,24 +11,23 @@ module.exports = {
       'lg': '1024px',
       'xl': '1280px',
       '2xl': '1440px',
-      extend: {},
-    },
-    fontFamily: {
-      'body': ['Karla', 'sans-serif'],
-      'headings':['DM Serif Display', 'serif'],
     },
     colors: {
       'primary': 'hsl(256, 26%, 20%)',
       'secondary': 'hsl(216, 30%, 68%)',
-      'violet':'hsl(270, 9%, 17%)',
+      'violet': 'hsl(270, 9%, 17%)',
       'gray': 'hsl(273, 4%, 51%)',
-      'white':'hsl(0, 0%, 98%)',
+      'white': 'hsl(0, 0%, 98%)',
     },
-      container: {
+    container: {
       center: true,
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+  purge: {
+    enabled: true,
+    content: [
+        './**/*.html'
+    ]
 }
-
-
+};
