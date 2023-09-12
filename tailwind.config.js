9428d71e5c5727a9ed14518806cb3139af083ae2
Dominik Beran <d.beran27@gmail.com>
2023-09-12 14:46:42 +0200

    project set-up

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    screens: {
+      'sm': '640px',
+      'md': '735px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1440px',
+      extend: {},
+    },
+    fontFamily: {
+      'body': ['Karla', 'sans-serif'],
+      'headings':['DM Serif Display', 'serif'],
+    },
+    colors: {
+      'primary': 'hsl(256, 26%, 20%)',
+      'secondary': 'hsl(216, 30%, 68%)',
+      'violet':'hsl(270, 9%, 17%)',
+      'gray': 'hsl(273, 4%, 51%)',
+      'white':'hsl(0, 0%, 98%)',
+    },
+      container: {
+      center: true,
+    },
+    plugins: [],
+  }
 }
+
 
